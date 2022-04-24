d5e7001895ed59bfa78c5a9b3e6ff2a655fb230b
s80679-17889 <s80679@beuth-hochschule.de>
2022-04-24 16:04:11 +0200

    mp4 player built in

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -15,37 +15,37 @@ function Overview() {
       title: 'first video',
       author: 'max mustermann',
       id: 1,
-      path: 'somewhere'
+      path: '../assets/sample.mp4'
     },
     {
       title: 'second video',
       author: 'anna musterfrau',
       id: 2,
-      path: 'soemwhere'
+      path: '../assets/sample.mp4'
     },
     {
       title: 'third video',
       author: 'john doe',
       id: 3,
-      path: 'somewhere'
+      path: '../assets/sample.mp4'
     },
     {
       title: 'third video',
       author: 'john doe',
-      id: 3,
-      path: 'somewhere'
+      id: 4,
+      path: '../assets/sample.mp4'
     },
     {
       title: 'third video',
       author: 'john doe',
-      id: 3,
-      path: 'somewhere'
+      id: 5,
+      path: '../assets/sample.mp4'
     },
     {
       title: 'third video',
       author: 'john doe',
-      id: 3,
-      path: 'somewhere'
+      id: 6,
+      path: '../assets/sample.mp4'
     },
   ]
 
@@ -53,8 +53,8 @@ function Overview() {
     <Container fluid>
       <Row xs={1} md={3} className="p-3">
       {videos.map(item => (
-        <LinkContainer to={`/video/${item.id}`}>
-        <Card style={{ width: '18rem' }} className="p-2 m-3" key={'VideoKey' + item.id}>
+        <LinkContainer to={`/video/${item.id}`} key={'LinkKey' + item.id}>
+        <Card style={{ width: '18rem' }} className="p-2 m-3" key={'CardKey' + item.id}>
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
             <Card.Title>{item.title}</Card.Title>
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Player } from 'video-react';
 
 function Video() {
+
+  const videos = [
+    {
+      title: 'first video',
+      author: 'max mustermann',
+      id: 1,
+      path: '../assets/sample.mp4'
+    },
+    {
+      title: 'second video',
+      author: 'anna musterfrau',
+      id: 2,
+      path: '../assets/sample.mp4'
+    },
+    {
+      title: 'third video',
+      author: 'john doe',
+      id: 3,
+      path: '../assets/sample.mp4'
+    },
+    {
+      title: 'third video',
+      author: 'john doe',
+      id: 4,
+      path: '../assets/sample.mp4'
+    },
+    {
+      title: 'third video',
+      author: 'john doe',
+      id: 5,
+      path: '../assets/sample.mp4'
+    },
+    {
+      title: 'third video',
+      author: 'john doe',
+      id: 6,
+      path: '../assets/sample.mp4'
+    },
+  ]
+
+  const id = window.location.href.split('/')[4]
+  let path = ''
+
+  for(let item in videos){
+    if(item.id === id){
+      console.log(item.id)
+      path = item.path
+      console.log(path)
+    }
+  }
+
   return (
-    <div>Video</div>
+    <Container className='p-3'>
+    <Player>
+      <source src="http://localhost:3000/videos/sample.mp4" />
+    </Player>
+    </Container>
   )
 }
 
